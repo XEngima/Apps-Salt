edbce18ddb45819675cae187cc267f6a4b43f463
SCBDEIS <SCBDEIS@CLD10302>
2021-06-29 15:40:38 +0200

    Added test project.

---
+++ Salt.Test/CryptographerTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Salt.Cypher;
+using System;
+
+namespace Salt.Test
+{
+    [TestClass]
+    public class CryptographerTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // A
+            var cryptographer = new Cryptographer();
+
+            // A
+            string encryptedString = cryptographer.Encrypt("abc", "aaa");
+
+            // Assert
+            Assert.AreEqual("bde", encryptedString);
+        }
+    }
+}
