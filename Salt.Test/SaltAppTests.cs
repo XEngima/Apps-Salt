33f3f5ef925764040371e8741fc0e9c1f1ec3e46
Eiserman Daniel IT UTV-4-Ö <daniel.eiserman@scb.se>
2021-11-23 15:42:24 +0100

    Refactored the SaltAppTests file and messed up some tests.

--- Salt.Test/SaltAppTests.cs
+++ Salt.Test/SaltAppTests.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Salt.Business;
-using Salt.Cypher;
 using Salt.Messages;
 using Salt.Contacts;
 using System;
@@ -13,20 +12,81 @@ namespace Salt.Test
     [TestClass]
     public class SaltAppTests
     {
+        [TestInitialize]
+        public void Initialize()
+        {
+            Cryptographer = new TestCryptographer();
+
+            // Create messages in the test message store
+
+            TobiasContactId = Guid.Parse("00000001-f760-4cf6-a84d-526397dc8b2a");
+            DanielContactId = Guid.Parse("00000002-f760-4cf6-a84d-526397dc8b2a");
+            SamuelContactId = Guid.Parse("00000003-f760-4cf6-a84d-526397dc8b2a");
+
+            var messageStoreItems = new List<IMessageStoreItem>();
+
+            var header = new MessageHeader
+            {
+                Date = new DateTime(2021, 01, 01, 12, 00, 00),
+                Sender = TobiasContactId,
+                Recipients = new List<Guid>() { DanielContactId }
+            };
+
+            var message = new Message
+            {
+                Subject = "A sign in the stars",
+                Content = "That's correct! This is awesome!"
+            };
+
+            messageStoreItems.Add(new MessageStoreItem(Guid.Parse("10000001-9575-4f71-ba28-cf09c5fdf200"), "DanielTobiasKey", 0, JsonConvert.SerializeObject(header), JsonConvert.SerializeObject(message)));
+
+            header = new MessageHeader
+            {
+                Date = new DateTime(2021, 01, 02, 12, 00, 00),
+                Sender = SamuelContactId,
+                Recipients = new List<Guid>() { DanielContactId }
+            };
+
+            message = new Message
+            {
+                Subject = "Lin Wood?",
+                Content = "Is he corrupt?"
+            };
+
+            messageStoreItems.Add(new MessageStoreItem(Guid.Parse("10000002-9575-4f71-ba28-cf09c5fdf200"), "DanielSamuelKey", 0, JsonConvert.SerializeObject(header), JsonConvert.SerializeObject(message)));
+
+            header = new MessageHeader
+            {
+                Date = new DateTime(2021, 01, 03, 12, 00, 00),
+                Sender = DanielContactId,
+                Recipients = new List<Guid>() { SamuelContactId }
+            };
+
+            message = new Message
+            {
+                Subject = "Re: Lin Wood?",
+                Content = "No, he's not corrupt!"
+            };
+
+            messageStoreItems.Add(new MessageStoreItem(Guid.Parse("10000003-9575-4f71-ba28-cf09c5fdf200"), "DanielSamuelKey", 0, JsonConvert.SerializeObject(header), JsonConvert.SerializeObject(message)));
+
+            MessageStore = new TestMessageStore
+            {
+                MessageStoreItems = messageStoreItems
+            };
+        }
+
+        private TestCryptographer Cryptographer { get; set; }
+        private TestMessageStore MessageStore { get; set; }
+
+        private Guid TobiasContactId { get; set; }
+        private Guid DanielContactId { get; set; }
+        private Guid SamuelContactId { get; set; }
+
         [TestMethod]
         public void EncryptedMessageInStore_GetMessageFromStore_GotDecryptedMessage()
         {
             // Arrange
-            var cryptographer = new TestCryptographer();
-
-            var messageStore = new TestMessageStore()
-            {
-                MessageStoreItems = new List<IMessageStoreItem>
-                {
-                    new MessageStoreItem(Guid.Parse("00000001-9575-4f71-ba28-cf09c5fdf200"), "MyKey123", 0, "", "ABCD")
-                }
-            };
-
             var keyStore = new TestKeyStore()
             {
                 Items = new List<TestKeyStoreItem>
@@ -35,7 +95,7 @@ namespace Salt.Test
                 }
             };
 
-            var saltApp = new SaltApp(null, messageStore, keyStore, cryptographer);
+            var saltApp = new SaltApp(null, MessageStore, keyStore, Cryptographer);
 
             // Act
             var message = saltApp.GetDecryptedMessage(Guid.Parse("00000001-9575-4f71-ba28-cf09c5fdf200"));
@@ -48,41 +108,16 @@ namespace Salt.Test
         public void EncryptedMessagesInStore_GetMessageHeadersByContactId_CorrectMessagesReturned()
         {
             // Arrange
-            var cryptographer = new TestCryptographer();
-
-            Guid tobiasContactId = Guid.Parse("00000001-f760-4cf6-a84d-526397dc8b2a");
-
             var contactStore = new TestContactStore()
             {
                 Contacts = new List<IContactStoreItem>
                 {
                     new ContactItem
                     {
-                        Id = tobiasContactId,
+                        Id = TobiasContactId,
                         Name = "Tobias",
                         KeyName = "MyKey123"
                     }
-                }
-            };
-
-            var header = new MessageHeader
-            {
-                Date = new DateTime(2021, 01, 01, 12, 00, 00),
-                Sender = tobiasContactId, // Tobias
-                Recipients = new List<Guid>()
-            };
-
-            var message = new Message
-            {
-                Subject = "Stjärntecknet",
-                Content = "Det stämmer! Det är mäktigt detta!"
-            };
-
-            var messageStore = new TestMessageStore()
-            {
-                MessageStoreItems = new List<IMessageStoreItem>
-                {
-                    new MessageStoreItem(Guid.Parse("00000001-9575-4f71-ba28-cf09c5fdf200"), "MyKey123", 0, JsonConvert.SerializeObject(header), JsonConvert.SerializeObject(message))
                 }
             };
 
@@ -94,10 +129,10 @@ namespace Salt.Test
                 }
             };
 
-            var saltApp = new SaltApp(contactStore, messageStore, keyStore, cryptographer);
+            var saltApp = new SaltApp(contactStore, MessageStore, keyStore, Cryptographer);
 
             // Act
-            var messageHeader = saltApp.GetMessageHeadersByContactId(tobiasContactId);
+            var messageHeader = saltApp.GetMessageHeadersByContactId(TobiasContactId);
 
             // Assert
             Assert.AreEqual(1, messageHeader.Count());
@@ -107,41 +142,16 @@ namespace Salt.Test
         public void EncryptedMessagesInStore_GetMessageStoreItemsByContactId_CorrectMessagesReturned()
         {
             // Arrange
-            var cryptographer = new TestCryptographer();
-
-            Guid tobiasContactId = Guid.Parse("00000001-f760-4cf6-a84d-526397dc8b2a");
-
             var contactStore = new TestContactStore()
             {
                 Contacts = new List<IContactStoreItem>
                 {
                     new ContactItem
                     {
-                        Id = tobiasContactId,
+                        Id = TobiasContactId,
                         Name = "Tobias",
                         KeyName = "MyKey123"
                     }
-                }
-            };
-
-            var header = new MessageHeader
-            {
-                Date = DateTime.Now,
-                Sender = tobiasContactId, // Tobias
-                Recipients = new List<Guid>()
-            };
-
-            var message = new Message
-            {
-                Subject = "Stjärntecknet",
-                Content = "Det stämmer! Det är mäktigt detta!"
-            };
-
-            var messageStore = new TestMessageStore()
-            {
-                MessageStoreItems = new List<IMessageStoreItem>
-                {
-                    new MessageStoreItem(Guid.Parse("00000001-9575-4f71-ba28-cf09c5fdf200"), "MyKey123", 0, JsonConvert.SerializeObject(header), JsonConvert.SerializeObject(message))
                 }
             };
 
@@ -153,10 +163,10 @@ namespace Salt.Test
                 }
             };
 
-            var saltApp = new SaltApp(contactStore, messageStore, keyStore, cryptographer);
+            var saltApp = new SaltApp(contactStore, MessageStore, keyStore, Cryptographer);
 
             // Act
-            var messageStoreItems = saltApp.GetMessageStoreItemsByContactId(tobiasContactId);
+            var messageStoreItems = saltApp.GetMessageStoreItemsByContactId(TobiasContactId);
 
             // Assert
             Assert.AreEqual(1, messageStoreItems.Count());
