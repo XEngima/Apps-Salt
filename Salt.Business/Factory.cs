0dabdc5bee885195f863d9aa339a704462b596fe
Eiserman Daniel IT/SV/VA-Ö <daniel.eiserman@scb.se>
2021-12-25 18:44:13 +0100

    Added initialization to memory message store in main environment.

--- Salt.Business/Factory.cs
+++ Salt.Business/Factory.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
 using Salt.Contacts;
 using Salt.Messages;
 
@@ -11,9 +13,80 @@ namespace Salt.Business
             return new FakeContactStore();
         }
 
+        private static MemoryMessageStore InitializeMemoryMessageStore()
+        {
+            // Create messages in the test message store
+
+            var tobiasContactId = Guid.Parse("00000001-f760-4cf6-a84d-526397dc8b2a");
+            var danielContactId = Guid.Parse("00000002-f760-4cf6-a84d-526397dc8b2a");
+            var samuelContactId = Guid.Parse("00000003-f760-4cf6-a84d-526397dc8b2a");
+
+            var messageStoreItems = new List<IMessageStoreItem>();
+
+            var messageTobiasToDanielId = Guid.Parse("10000001-9575-4f71-ba28-cf09c5fdf200");
+            var messageSamuelToDanielId = Guid.Parse("10000002-9575-4f71-ba28-cf09c5fdf200");
+            var messageDanielToSamuelId = Guid.Parse("10000003-9575-4f71-ba28-cf09c5fdf200");
+
+            // Message from Tobias to Daniel
+
+            var header = new MessageHeader
+            {
+                Date = new DateTime(2021, 01, 01, 12, 00, 00),
+                Sender = tobiasContactId,
+                Recipients = new List<Guid>() { danielContactId }
+            };
+
+            var message = new Message
+            {
+                Subject = "A SIGN IN THE STARS",
+                Content = "THAT'S CORRECT! THIS IS AWESOME!"
+            };
+
+            messageStoreItems.Add(new MessageStoreItem(messageTobiasToDanielId, "DanielTobiasKey", 0, JsonConvert.SerializeObject(header), JsonConvert.SerializeObject(message)));
+
+            // Message from Samuel to Daniel
+
+            header = new MessageHeader
+            {
+                Date = new DateTime(2021, 01, 02, 12, 00, 00),
+                Sender = samuelContactId,
+                Recipients = new List<Guid>() { danielContactId }
+            };
+
+            message = new Message
+            {
+                Subject = "LIN WOOD?",
+                Content = "IS HE CORRUPT?"
+            };
+
+            messageStoreItems.Add(new MessageStoreItem(messageSamuelToDanielId, "DanielSamuelKey", 0, JsonConvert.SerializeObject(header), JsonConvert.SerializeObject(message)));
+
+            // Message from Daniel to Samuel
+
+            header = new MessageHeader
+            {
+                Date = new DateTime(2021, 01, 03, 12, 00, 00),
+                Sender = danielContactId,
+                Recipients = new List<Guid>() { samuelContactId }
+            };
+
+            message = new Message
+            {
+                Subject = "RE: LIN WOOD?",
+                Content = "NO, HE IS NOT CORRUPT!"
+            };
+
+            messageStoreItems.Add(new MessageStoreItem(messageDanielToSamuelId, "DanielSamuelKey", 0, JsonConvert.SerializeObject(header), JsonConvert.SerializeObject(message)));
+
+            return new MemoryMessageStore
+            {
+                MessageStoreItems = messageStoreItems
+            };
+        }
+
         public static IMessageStore CreateMessageStore()
         {
-            return new MemoryMessageStore();
+            return InitializeMemoryMessageStore();
         }
     }
 }
