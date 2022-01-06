0101373b23bf4805ea5edacfb09a8679ac6fc487
Daniel Eiserman <daniel.eiserman@protonmail.com>
2022-01-06 13:14:03 +0100

    Added messaging window.

---
+++ Salt.Gui/MessagingWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Salt.Gui
+{
+    /// <summary>
+    /// Interaction logic for MessagingWindow.xaml
+    /// </summary>
+    public partial class MessagingWindow : Window
+    {
+        public MessagingWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Salt.Gui/MessagingWindowViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salt
+{
+    public class MessagingWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(PropertyChangedEventArgs e)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, e);
+            }
+        }
+
+
+        public void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
