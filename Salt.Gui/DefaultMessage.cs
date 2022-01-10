7bd1203263cb21845e7eee28f708cb7e39f9e2c1
Daniel Eiserman <daniel.eiserman@protonmail.com>
2022-01-10 22:50:43 +0100

    Fixed some typos in the default message.

--- Salt.Gui/DefaultMessage.cs
+++ Salt.Gui/DefaultMessage.cs
@@ -10,7 +10,7 @@ namespace Salt.Gui
     {
         public static string GetInstructionsMessage()
         {
-            return @"Hi and welcome as a Salt user!
+            return @"Hi and welcome new Salt user!
 
 Salt is - if correctly used - a 100% safe messaging tool for encrypted communications. Here are some instructions to get you started.
 
@@ -38,7 +38,7 @@ Each key is a file, and the file name (without the .key extension) is the key's 
 
 The contact store is by default created as a folder in the application's working directory.
 
-Each contact is an XML file, containing the name and the ID of a contact. The key to use in conversations with this contact need also be written in the file. Check the default Me contact for an example. The best and safest practise is to use one unique key per contact.
+Each contact is an XML file, containing the name and the ID of a contact. The key to use in conversations with this contact need also be written in the file. Check the default Me contact for an example. The best and safest practice is to use one unique key per contact.
 
 The name of a contact XML file can be whatever, but best practice would be to name the file after the contact.
 
@@ -48,23 +48,23 @@ The name of a contact XML file can be whatever, but best practice would be to na
 
 The message store is by default created as a folder in the application's working directory.
 
-The messages are stored as XML files. One file per message. These files are encrypted, and while you can read som meta data in the file - e.g. which key is used - the sender, reciever, subject and message body is unreadable unless you have access to the key.
+The messages are stored as XML files. One file per message. These files are encrypted, and while you can read some meta data in the file - e.g. which key is used - the sender, receiver, subject and message body is unreadable unless you have access to the key.
 
 ===================================
- 5. Sending And Recieving Messages
+ 5. Sending and Receiving Messages
 ===================================
 
-To send a message, select New Message from the Messaging menu. Write the name of the contact as it is written in the Contact file. Then write a subject and the message body and press the Send button. The message is now stored in the message store. Find it and send it to the recipient in any way you want. By email, cloud storage, or type it in a snail mail (the last alternative not recommended). Noone will be able to decrypt it without the key, not even the military's smartest AI running on the world's fastest super computer.
+To send a message, select New Message from the Messaging menu. Write the name of the contact as it is written in the Contact file. Then write a subject and the message body and press the Send button. The message is now stored in the message store. Find it and send it to the recipient in any way you want. By email, cloud storage, or type it in a snail mail (the last alternative not recommended). No one will be able to decrypt it without the key, not even the military's smartest AI running on the world's fastest super computer.
 
-When you recieve a message (let's say by email), download the encrypted XML message file and put it into your message store. When you start up Salt it will appear automatically. However, it will not appear if you do not have the contact in you contact store or if you are missing the necessary key in the key store.
+When you receive a message (let's say by email), download the encrypted XML message file and put it into your message store. When you start up Salt it will appear automatically. However, it will not appear if you do not have the contact in you contact store or if you are missing the necessary key in the key store.
 
 =======================
  6. Why Is It So Safe?
 =======================
 
-The algorithm used is a simple ceasar crypto in which each character represents a number. Each character in a message is then 'added' to the corresponding character in the key, which gives a new character - the encrypted character.
+The algorithm used is a simple Caesar crypto in which each character represents a number. Each character in a message is then 'added' to the corresponding character in the key, which gives a new character - the encrypted character.
 
-This crypto is not safe if you use a short key. However, if you use a key that is of equal length as the message, noone will be able to 'hack' it. The reason is that different keys decrypts to different perfectly readable (or unreadable) messages. A 'hacker' will never be able to decide if your message is 'I will blow it up' or 'Want some coffee?'. One key will give the former, and another will give the latter. Of course, if you reuse any part of a key in another message, the communication can be hacked. The Salt application handles this, but to be safe you can double check the key positions used - they are written in the message file.";
+This crypto is not safe if you use a short key. However, if you use a key that is of equal length as the message, no one will be able to 'hack' it. The reason is that different keys decrypts to different perfectly readable (or unreadable) messages. A 'hacker' will never be able to decide if your message is 'I will blow it up' or 'Want some coffee?'. One key will give the former, and another will give the latter. Of course, if you reuse any part of a key in another message, the communication can be hacked. The Salt application handles this, but to be safe you can double check the key positions used - they are written in the message file.";
         }
     }
 }
